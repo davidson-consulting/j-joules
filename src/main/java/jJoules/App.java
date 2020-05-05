ac34fdfadb910394c307e8899067997ed368aea6
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-05-05 12:22:23 +0200

    Initial commit

---
+++ src/main/java/jJoules/App.java
@@ -0,0 +1,13 @@
+package jJoules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
