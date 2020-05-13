2be31d0d6f53fdbcacb950b2792774bf9fe6dabc
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-05-13 19:07:18 +0200

    fixing test for device energy consumption

--- src/test/java/jJoules/energyDevice/rapl/RaplDeviceTest.java
+++ src/test/java/jJoules/energyDevice/rapl/RaplDeviceTest.java
@@ -6,7 +6,14 @@ package jJoules.energyDevice.rapl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import jJoules.exceptions.DeviceNotConfiguredException;
+import jJoules.exceptions.NoSuchDomainException;
+import jJoules.exceptions.NoSuchEnergyDeviceException;
 
 /**
  * @author sanoussy
@@ -16,14 +23,19 @@ class RaplDeviceTest {
 
 	private RaplDevice raplDevice;
 		
-	public void initDevice() {
+	@BeforeEach
+	public void initDevice() throws NoSuchEnergyDeviceException {
 		this.raplDevice = new RaplDevice();
 	}
 	
 	@Test
-	public void getConsumedEnergy() {
-		double consumedEnergy = this.raplDevice.getEnergyConsumed();
-		
-		assertThat(consumedEnergy).isGreaterThanOrEqualTo(0);
+	public void getConsumedEnergy() throws DeviceNotConfiguredException, NoSuchDomainException {
+		raplDevice.configure(raplDevice.getAvailableDomains());
+		ArrayList<Double> consumedEnergy = raplDevice.getEnergyConsumed();
+
+		for(Double d : consumedEnergy) {
+			System.out.println(d);
+			assertThat(d).isGreaterThan(0);
+		}
 	}
 }
