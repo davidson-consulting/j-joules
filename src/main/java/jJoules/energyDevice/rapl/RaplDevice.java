7ad0bc668c7a2be126c4e3c6ec70b1af6f861045
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-05-13 19:27:57 +0200

    coding methods

--- src/main/java/jJoules/energyDevice/rapl/RaplDevice.java
+++ src/main/java/jJoules/energyDevice/rapl/RaplDevice.java
@@ -3,10 +3,14 @@
  */
 package jJoules.energyDevice.rapl;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import jJoules.energyDevice.EnergyDevice;
+import jJoules.energyDomain.EnergyDomain;
 import jJoules.energyDomain.rapl.RaplDomain;
+import jJoules.energyDomain.rapl.RaplPackageDomain;
+import jJoules.exceptions.DeviceNotConfiguredException;
 import jJoules.exceptions.NoSuchEnergyDeviceException;
 
 /**
@@ -14,45 +18,86 @@ import jJoules.exceptions.NoSuchEnergyDeviceException;
  *
  */
 public class RaplDevice extends EnergyDevice{
-	
-	private ArrayList<RaplDomain> configuredDomains;
-	private ArrayList<RaplDomain> availableDomains;
 
 	/**
+	 * @throws NoSuchEnergyDeviceException 
 	 * 
 	 */
-	public RaplDevice() {
+	public RaplDevice() throws NoSuchEnergyDeviceException {
+		super();
 	}
 	
 	
-	/**
-	 * @param pathName path to check if it is existent path
-	 * @return true if rapl path exist and false otherwise
-	 */
-	public static boolean raplPathExist(String pathName) {
-		return false;
-	}
+//	/**
+//	 * @param pathName path to check if it is existent path
+//	 * @return true if rapl path exist and false otherwise
+//	 */
+//	public static boolean raplPathExist(String pathName) {
+//		return false;
+//	}
 	
-	/**
-	 * @return all available domain that could be monitored on the device
-	 */
-	public ArrayList<RaplDomain> availableDomains() throws NoSuchEnergyDeviceException {
-		return null;
-	}
-	
-	/**
-	 * @param domains all domains to configure
-	 */
-	public void configure(ArrayList<RaplDomain> domains) {
+	@Override
+	public ArrayList<EnergyDomain> availableDomains() throws NoSuchEnergyDeviceException {
+		if(! new File(RaplDomain.RAPL_PATH_NAME).exists()) 
+			throw new NoSuchEnergyDeviceException();
+		ArrayList<EnergyDomain> availDomains = new ArrayList<EnergyDomain>();
 		
-	};
+		availDomains.addAll(availablePKGDomains());
+		return availDomains;
+	}
 	
-	/**
-	 * @return the energy consumed by device
-	 */
-	public long getEnergyConsumed() {
-		return 0;
-	};
+	public static ArrayList<EnergyDomain> availablePKGDomains() {
+		ArrayList<EnergyDomain> pkgDomain = new ArrayList<EnergyDomain>();
+		for(int id=0; id < getSocketIds();id++ ) {
+			String domainNameFilePath = RaplDomain.RAPL_PATH_NAME+ "/intel-rapl:" + id+"/name";
+			if (new File(domainNameFilePath).exists()) {
+				String domainName = RaplDomain.openAndReadFile(domainNameFilePath);
+				if(domainName.equalsIgnoreCase("package-"+id))
+					pkgDomain.add(new RaplPackageDomain(id));
+			}
+		}return pkgDomain;
+	}
+	
+    private static int getSocketIds() {
+		int socketId = 0;
+		while (true) {
+			String pathName = RaplDomain.RAPL_PATH_NAME+"/intel-rapl:" + socketId;
+			if (new File(pathName).exists()) 
+				socketId += 1;
+			else return socketId;
+		}
+	}
+
+
+//	private static ArrayList<EnergyDomain> availableCoreDomains() {
+//		return null;
+//	}
+//	private static ArrayList<EnergyDomain> availableUncoreDomains() {
+//		return null;
+//	}
+//	private static ArrayList<EnergyDomain> availableDramDomains() {
+//		return null;
+//	}
+
+
+//	public int[] getDomainOnSocket(int domainSocket,String domainName){
+//		int domainId = 0;
+//		while (true) {
+//			String domainPath = RaplDomain.RAPL_PATH_NAME + "/intel-rapl:"+domainId+"/name";
+//			if (RaplDomain.openAndReadFile(domainPath).equals(domainName)) {
+//				
+//			}
+//		}
+//}
+	
+	@Override
+	public ArrayList<Double> getEnergyConsumed() throws DeviceNotConfiguredException {
+		ArrayList<Double> energyConsumed = new ArrayList<Double>();
+		for(EnergyDomain domain : this.getConfiguredDomains()) {
+			energyConsumed.add(domain.getEneregyConsumed());
+		}
+		return energyConsumed;
+	}
 	
 
 }
