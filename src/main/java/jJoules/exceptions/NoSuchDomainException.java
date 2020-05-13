48c0eb2018ca69f0050d0d19bde46a77c96b01eb
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-05-13 15:39:26 +0200

    exception

---
+++ src/main/java/jJoules/exceptions/NoSuchDomainException.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package jJoules.exceptions;
+
+/**
+ * @author sanoussy
+ *
+ */
+public class NoSuchDomainException extends JJoulesException {
+
+	/**
+	 * 
+	 */
+	public NoSuchDomainException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public NoSuchDomainException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
