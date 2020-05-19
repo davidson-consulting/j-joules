509fec29f04aef4ec4da44eec00d8fe2048fa900
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-05-19 13:27:40 +0200

    for test

---
+++ src/main/java/jJoules/Main.java
@@ -0,0 +1,35 @@
+package jJoules;
+
+import jJoules.energyDevice.EnergyDevice;
+import jJoules.energyDevice.rapl.RaplDevice;
+import jJoules.energyDomain.rapl.RaplPackageDomain;
+
+public class Main {
+
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		RaplPackageDomain pkg = new RaplPackageDomain(0);
+		EnergyMesureIt mesureIt = new EnergyMesureIt(pkg);
+		
+		mesureIt.begin();
+		System.out.println("before => "+mesureIt.getEnergyBefore());
+		
+		for(int i=0;i<10000; i++) {}
+		
+		double diff = mesureIt.end();
+		System.out.println("after => "+ mesureIt.getEnergyAfter());
+		System.out.println("diff => "+diff);
+		
+		try {
+			EnergyDevice device = new RaplDevice();
+			device.configure(null);
+			System.out.println(device.getAvailableDomains());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
